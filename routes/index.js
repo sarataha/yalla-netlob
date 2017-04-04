fd86fed8b0b2b7d17baea58085a99019edaba844
Dina <dinaibrahim8.di@gmail.com>
2017-04-04 15:58:33 +0200

    regiteration login and tzbitat el header w files el bootstarp

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Yalla Netlob | Login' });
 });
+router.get('/register', function(req, res, next) {
+  res.render('Registration', { title: 'Yalla Netlob | Registration' });
+});
+
 
 /* GET friends page. */
 router.get('/friends', function(req, res, next) {

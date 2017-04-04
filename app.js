e206b4ff1910e6a52d332e73de8840a63edce594
Sara Taha <st.elzayat@gmail.com>
2017-04-04 12:06:31 +0200

    update header

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
-// var passport = require('passport');
-// var session = require('express-session');
+var passport = require('passport');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
--- routes/friends.js
+++ routes/friends.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET friends page. */
 router.get('/', function(req, res, next) {
-  res.render('friends', { title: 'Yalla Netlob | Friends' });
+  res.render('friends', { title: 'Yalla Netlob | Friends', name: 'Sara' });
 });
 
 module.exports = router;

5ecab167865611b32e0eeaf8a7800da755319702
Sara Taha <st.elzayat@gmail.com>
2017-04-05 15:20:48 +0200

    add database connection, user_find and insert_user to users model

--- models/users.js
+++ models/users.js
@@ -1,4 +1,8 @@
-var mysql      = require('mysql');
+// ./models/users.js
+
+var mysql = require('mysql');
+var bcrypt = require('bcrypt-nodejs');
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -8,19 +12,54 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-exports.find_user = function() {
-	var query = "SELECT * FROM users WHERE email ='" + email + "' AND password='"+password+"'"
-	connection.query(query, function(err, rows, fields) {
-		if(!err)
-			console.log("");
-		else
-			console.log("");
-	});
+// for login
+var find_user = function(email,password) {
+  var query = "SELECT * FROM users WHERE email ='" + email + "' AND password='"+password+"'"
+  connection.query(query, function(err, rows, fields) {
+    if(!err)
+      if (rows.length > 0)
+        console.log('User found: ', rows);
+      else
+        console.log('invalid user data');
+    else
+      console.log("");
+  });
 }
 
-connection.query('SELECT * from users', function(err, rows, fields) {
+// for registeration
+var insert_user = function(username, email, password, avatar_url) {
+ var check_if_user_exists = find_user(email, password);
+ if (check_if_user_exists) {
+   console.log('user already exists');
+ }
+ else {
+   var user = {
+         email: "sara@sara.com",
+         password: "whatever",
+         avatar_url: ""
+     };
+   var query = "INSERT INTO users SET ?";
+   connection.query(query, user, function(err, result) {
+     if (err)
+       console.log(err);
+     else
+       console.log(result);
+   });
+ }
+}
+
+// find user's friends
+var find_friends = function() {
+
+}
+
+// test connection
+connection.query('SELECT * from users WHERE email = "Jonas8@example.org"', function(err, rows, fields) {
   if (!err)
-    console.log('The solution is: ', rows);
+    if (rows.length > 0)
+      console.log('User found: ', rows[0].user_name);
+  else
+    console.log('no user found');
   else
     console.log('Error while performing Query.');
 });

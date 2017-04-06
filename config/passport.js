2fbd722a1fedb56b912ab3f5a18313d7f12775f7
Sara Taha <st.elzayat@gmail.com>
2017-04-07 00:22:25 +0200

    remove unecessary stuff

--- config/passport.js
+++ config/passport.js
@@ -16,8 +16,6 @@ module.exports = function(passport) {
     // =========================================================================
     // passport session setup ==================================================
     // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
 
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
@@ -34,28 +32,25 @@ module.exports = function(passport) {
     // =========================================================================
     // LOCAL SIGNUP ============================================================
     // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
 
     passport.use(
         'local-signup',
         new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField : 'username',
+            usernameField : 'email',
             passwordField : 'password',
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
-        function(req, username, password, done) {
+        function(req, email, password, done) {
             // find a user whose email is the same as the forms email
-            // we are checking to see if the user trying to login already exists
-            connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows) {
+            connection.query("SELECT * FROM users WHERE email = ?",[email], function(err, rows) {
                 if (err)
                     return done(err);
                 if (rows.length) {
                     return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
                 } else {
-                    // if there is no user with that username
+                    // if there is no user with that email
                     // create the user
+                    // DOES NOT WORK
                     var newUserMysql = {
                         username: username,
                         password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
@@ -76,19 +71,16 @@ module.exports = function(passport) {
     // =========================================================================
     // LOCAL LOGIN =============================================================
     // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
 
     passport.use(
         'local-login',
         new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField : 'username',
+            usernameField : 'email',
             passwordField : 'password',
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
-        function(req, username, password, done) { // callback with email and password from our form
-            connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows){
+        function(req, email, password, done) { // callback with email and password from our form
+            connection.query("SELECT * FROM users WHERE email = ?",[email], function(err, rows){
                 if (err)
                     return done(err);
                 if (!rows.length) {
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,5 @@ router.get('/', function(req, res, next) {
   	username: 'Sara'
   });
 });
-router.get('/register', function(req, res, next) {
-  res.render('Registration', { title: 'Registration' });
-});
-
-/* GET login page. */
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login' });
-});
 
 module.exports = router;

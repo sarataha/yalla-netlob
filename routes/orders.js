4f8990e08b63d2bcb5d336335080a2959a82c3e3
wafaa mahmoud <wafaa_mahmoud94x@yahoo.com>
2017-04-08 00:50:41 +0200

    orders

--- routes/orders.js
+++ routes/orders.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'yala_netlob_development'
+});
+
+connection.connect();
 
 /* GET orders page. */
 router.get('/', function(req, res, next) {
+  var query="select meal_type,order_status,resturant from orders";
+  connection.query(query,function(err,row,fields){
+    if(!err)
+      console.log(row[0]);
+    else {
+      console.log("error");
+    }
+  });
   res.render('orders', {
   	title: 'Orders',
   	username: 'Sara'

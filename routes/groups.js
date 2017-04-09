3cd0ef63cc33bdd569684c99b8bd61bea1b7232e
mariamgamal <Mariam@Mariams-MacBook-Pro.local>
2017-04-09 09:11:02 +0200

    groups tany bs fi azma f el routes

--- routes/groups.js
+++ routes/groups.js
@@ -2,7 +2,6 @@ var mysql=require('mysql');
 var router = require('express').Router();
 var dbconfig = require('../models/groups');
 var connection = mysql.createConnection(dbconfig.connection);
-
 var bodyParser=require('body-parser');
 var middlewareBodyParser=bodyParser.urlencoded({extended:false})
 var dbconfig = require('../models/groups');
@@ -12,9 +11,25 @@ var dbconfig = require('../models/groups');
 //   resp.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
 //   next()
 // });
-
 connection.query('USE ' + dbconfig.database);
 
+router.get("/",function(req,respo){
+  console.log("*********");
+  connection.query("SELECT * FROM groups ", function(err, rows) {
+
+      if (err)
+          return done(err);
+      if (rows.length) {
+        for(i=0;i<rows.length;i++)
+        {console.log("group name: "+rows[i].group_name);
+          $("#groupsNames").innerHTML+="<li>'"+rows[i].group_name+"'</li>";
+            }
+      } else {
+
+
+      }
+  });
+});
 router.post("/add",middlewareBodyParser,function(req,respo){
   console.log("groupnameee"+req.body.name);
   console.log("groupnameee"+req.body.name);
@@ -27,16 +42,12 @@ router.post("/add",middlewareBodyParser,function(req,respo){
       if (rows.length) {
         console.log("That group already exist");
       } else {
-          // var newGroupMysql = {
-          //   groupname: req.body.name,
-          //   groupadmin: req.body.user_id,
-          // };
 
           var insertQuery = "INSERT INTO groups ( group_name, group_admin ) values (?,?)";
 
           connection.query(insertQuery,[groupname,groupadmin],function(err, rows) {
               //newGroupMysql.id = rows.insertId;
-
+              respo.render('groups.ejs');
               console.log("DONE");
           });
       }

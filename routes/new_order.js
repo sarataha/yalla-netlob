0e9624f40d813ef230faf0610d29a9d3a10c2519
wafaa mahmoud <wafaa_mahmoud94x@yahoo.com>
2017-04-15 05:23:41 +0200

    add goup and friend to new_order

--- routes/new_order.js
+++ routes/new_order.js
@@ -37,14 +37,19 @@ router.post('/',middlewareBodyParser,function(req, res) {
     email=req.body.member_email;
     console.log(req.body.member_email);
     var member_id;
-    var query="select user_id from users where email='"+email+"'";
+    var friend_name;
+    var query="select user_id,user_name from users where email='"+email+"'";
     connection.query(query,function(err,row){
       if(!err){
         console.log("rowww emaiiiiil");
-        console.log(row[0].user_id);
-        member_id=row[0].user_id;
-        console.log("Member id ###########");
-        console.log(member_id);
+        console.log(row);
+        //console.log(row[0].user_id);
+        if(row.length>0){
+          member_id=row[0].user_id;
+          friend_name=row[0].user_name;
+          console.log("Member id ###########");
+          console.log(member_id);
+        }
       }else {
         console.log(err);
       }
@@ -53,7 +58,9 @@ router.post('/',middlewareBodyParser,function(req, res) {
         if(!err){
           console.log(row);
           if(row.length>0){
-            res.send({message:"is_friend"});
+            res.send({message:"is_friend",friend_name:friend_name,friend_id:member_id});
+          }else{
+            res.send({message:"not_friend"});
           }
         }else {
           console.log(err);
@@ -61,8 +68,38 @@ router.post('/',middlewareBodyParser,function(req, res) {
       });
     });
   }
-  else {
-    console.log(req.body.member_name);
+  else if(req.body.member_type=="group"){
+    group_name=req.body.member_name;
+    user_id=req.user.user_id;
+    var group_id;
+    console.log("into grooooooooooooup");
+    console.log(group_name);
+    console.log(user_id);
+    var query="select group_id from groups where group_admin='"+user_id+"' and group_name='"+group_name+"'";
+    connection.query(query,function(err,row){
+      if(!err){
+        console.log(row);
+        if(row.length>0){
+          group_id=row[0].group_id;
+          console.log(group_id);
+          var nquery="select users.user_name ,users.user_id from users,group_members where users.user_id=group_members.user_id and group_id='"+group_id+"'";
+          connection.query(nquery,function(error,nrow){
+            if(!error){
+              console.log(nrow);
+              if(nrow.length>0)
+                res.send({message:"is_group",group_members:nrow});
+            }else{
+              console.log(error);
+            }
+          });
+
+        }else{
+          res.send({message:"not_group"});
+        }
+      }else {
+        console.log(err);
+      }
+    });
   }
 
 });
